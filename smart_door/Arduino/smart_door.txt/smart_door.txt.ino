95573066b2a9d7e078eef2da38dc731a1acb1e1a
ManAvoCoder <webCodeNSK@gmail.com>
2022-01-21 19:40:01 +0700

    first  smart door
    doooooooooooooor

---
+++ smart_door/Arduino/smart_door.txt/smart_door.txt.ino
@@ -0,0 +1,10 @@
+int pinNum = 11;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pinNum,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(pinNum,HIGH);
+}
